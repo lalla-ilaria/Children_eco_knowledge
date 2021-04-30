fe21ac2b0ae436a765f725d00191ec54a8a7ff40
Ilaria Pretelli <ilaria_pretelli@ecocn04.eva.mpg.de>
2021-04-30 10:43:13 +0200

    meet Richard 04.30.2020

--- 3_Analysis/2_results_exploration.R
+++ 3_Analysis/2_results_exploration.R
@@ -1,3 +1,121 @@
+###########
+#FUNCTIONS#
+###########
+#colors
+d$sex_col <- ifelse(d$S == "m", "darkblue", "darkred")#assign color to each sex
+
+
+#plot age and sex regressions
+plot_age_sex <- function( post , d, dot_col ){
+  A_seq <- c( -2 : 3)#vector to loop over
+  mus1 <- matrix(nrow = 3000, ncol = length(A_seq)) #create empty matrix to store the fit model over the sequence of data
+  for (i in 1:length(A_seq)) {
+    mus1[,i] <- apply(post$aK, 1, mean) + post$aS[,1,] + post$bA[,1,] * A_seq[i]                 #calculate regression over the sequence of data A_seq, given the posterior
+  } #aK[i,j] + aS[S[i],j] + bA[S[i],j]*A[i]
+  mus1.mean <- apply(mus1, 2, mean) #calculate average regression line
+  mus1.PI <- apply(mus1, 2, PI) #calculate compatibility intervals
+  mus2 <- matrix(nrow = 3000, ncol = length(A_seq)) #create empty matrix to store the fit model over the sequence of data
+  for (i in 1:length(A_seq)) {
+    mus2[,i] <- apply(post$aK, 1, mean) + post$aS[,2,] +  post$bA[,2,] * A_seq[i]                 #calculate regression over the sequence of data A_seq, given the posterior
+  } #aK[i,j] + aS[S[i],j] + bA[S[i],j]*A[i]
+  mus2.mean <- apply(mus2, 2, mean)
+  mus2.PI <- apply(mus2, 2, PI)
+  #plot
+  plot( d$A[d$A <= 50], apply(post$K, 2, mean), 
+        xlab = "Age", ylab = "Knowledge", xaxt='n', col = dot_col )
+  axis(1, c(5, 10, 15, 20, 25), at = )
+  A_seq_real <- A_seq  * sd(d$A[d$A <= 50]) + mean(d$A[d$A <= 50])
+  lines(A_seq_real, mus1.mean, col = "darkblue")
+  shade(mus1.PI, A_seq_real, col = col.alpha("darkblue", 0.2))
+  lines(A_seq_real, mus2.mean, col = "darkred")
+  shade(mus2.PI, A_seq_real, col =  col.alpha("darkred", 0.2))
+  
+}
+
+
+####################
+#LINEAR AGE AND SEX#
+####################
+post_l <- extract.samples(m_lin) #extract samples
+
+plot_age_sex(post = post_l, d = d, dot_col = d$sex_col)
+
+######################
+#ADDING OTHER FACTORS#
+######################
+
+############
+#ACTIVITIES#
+############
+
+plot (precis (m_act, 3, pars = "aAM"))
+axis(2, at=10:1, labels=colnames(d$am), par(las=1))
+
+
+post_a <- extract.samples(m_act) #extract samples
+
+plot_age_sex(post = post_a, d = d, dot_col = d$sex_col)
+
+##########
+#FAMILIES#
+##########
+plot(precis(m_fam, 3, pars = "aH"))
+
+post_f <- extract.samples(m_fam)
+
+plot_age_sex(post = post_f, d = d, dot_col = as.character(d$HH))
+
+
+###########
+#SCHOOLING#
+###########
+precis(m_sch, 3, pars = "bSY")
+
+#plot effect of schooling
+post_s <- extract.samples(m_sch) #extract samples
+dot_col <- d$sex_col
+A_seq <- c( -2 : 3)#vector to loop over
+mus1 <- matrix(nrow = 3000, ncol = length(A_seq)) #create empty matrix to store the fit model over the sequence of data
+for (i in 1:length(A_seq)) {
+  mus1[,i] <- apply(post_s$aK, 1, mean) + post_s$aS[,1,] + post_s$bA[,1,] * A_seq[i]                #calculate regression over the sequence of data A_seq, given the post_serior
+} #aK[i,j] + aS[S[i],j] + bA[S[i],j]*A[i]
+mus1.mean <- apply(mus1, 2, mean) #calculate average regression line
+mus1.PI <- apply(mus1, 2, PI) #calculate compatibility intervals
+mus2 <- matrix(nrow = 3000, ncol = length(A_seq)) #create empty matrix to store the fit model over the sequence of data
+for (i in 1:length(A_seq)) {
+  mus2[,i] <- apply(post_s$aK, 1, mean) + post_s$aS[,2,] +  post_s$bA[,2,] * A_seq[i]  #need to get the SY from the correlated SY to each A
+} #aK[i,j] + aS[S[i],j] + bA[S[i],j]*A[i] 
+mus2.mean <- apply(mus2, 2, mean)
+mus2.PI <- apply(mus2, 2, PI)
+
+mus1.1 <- matrix(nrow = 3000, ncol = length(A_seq)) #create empty matrix to store the fit model over the sequence of data
+for (i in 1:length(A_seq)) {
+  mus1.1[,i] <- apply(post_s$aK, 1, mean) + post_s$aS[,1,] + post_s$bA[,1,] * A_seq[i] + post_s$bSY[,1,]*A_seq[i]                #calculate regression over the sequence of data A_seq, given the post_serior
+} #aK[i,j] + aS[S[i],j] + bA[S[i],j]*A[i] + bSY[S[i],j]*SY[i]
+mus1.1.mean <- apply(mus1.1, 2, mean) #calculate average regression line
+mus1.1.PI <- apply(mus1.1, 2, PI) #calculate compatibility intervals
+mus2.1 <- matrix(nrow = 3000, ncol = length(A_seq)) #create empty matrix to store the fit model over the sequence of data
+for (i in 1:length(A_seq)) {
+  mus2.1[,i] <- apply(post_s$aK, 1, mean) + post_s$aS[,2,] +  post_s$bA[,2,] * A_seq[i] + post_s$bSY[,2,]*A_seq[i]   #need to get the SY from the correlated SY to each A
+} #aK[i,j] + aS[S[i],j] + bA[S[i],j]*A[i] + bSY[S[i],j]*SY[i]
+mus2.1.mean <- apply(mus2.1, 2, mean)
+mus2.1.PI <- apply(mus2.1, 2, PI)
+
+#plot
+plot( d$A[d$A <= 50], apply(post_s$K, 2, mean), 
+      xlab = "Age", ylab = "Knowledge", xaxt='n', col = dot_col )
+axis(1, c(5, 10, 15, 20, 25), at = )
+A_seq_real <- A_seq  * sd(d$A[d$A <= 50]) + mean(d$A[d$A <= 50])
+lines(A_seq_real, mus1.mean, col = "darkblue")
+shade(mus1.PI, A_seq_real, col = col.alpha("darkblue", 0.2))
+lines(A_seq_real, mus2.mean, col = "darkred")
+shade(mus2.PI, A_seq_real, col =  col.alpha("darkred", 0.2))
+lines(A_seq_real, mus1.1.mean, col = "blue")
+shade(mus1.1.PI, A_seq_real, col = col.alpha("blue", 0.2))
+lines(A_seq_real, mus2.1.mean, col = "red")
+shade(mus2.1.PI, A_seq_real, col =  col.alpha("red", 0.2))
+
+
 #######################
 #ORDERED CATEGORICAL A#
 #######################
